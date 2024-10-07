d6718abfd9ad55f47d8bf6592e93df30c9c75878
okc27 <osamakhursheedc273@gmail.com>
2024-10-07 17:39:15 +0500

    7-10 update 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,46 @@
 import React from 'react';
-import './Navbar.css'; // Make sure to import your CSS file for custom styles
+import { Navbar, Nav, Form } from 'react-bootstrap';
 
-const Navbar = ({ svgColor, setSvgColor, bgColor, setBgColor }) => {
+const CustomNavbar = ({ svgColor, setSvgColor, bgColor, setBgColor, searchTerm, setSearchTerm }) => {
+  const handleSvgColorChange = (event) => {
+    setSvgColor(event.target.value);
+  };
+
+  const handleBgColorChange = (event) => {
+    setBgColor(event.target.value); // Update background color state
+  };
+
   return (
-    <nav className="navbar navbar-light" style={{ backgroundColor: '#add8e6' }}>
-      <div className="container-fluid">
-        <div className="navbar-brand " onClick={() => console.log("Brand clicked!")}>
-          the2px.com
-        </div>
-        <div className="color-picker d-flex justify-content-center"> {/* Center color pickers */}
-          <label htmlFor="svg-color-picker" className="me-2">SVG Color:</label>
-          <input
-            type="color"
-            id="svg-color-picker"
-            value={svgColor}
-            onChange={(e) => setSvgColor(e.target.value)}
-          />
-          <label htmlFor="background-color-picker" className="mx-2">Background Color:</label>
-          <input
-            type="color"
-            id="background-color-picker"
-            value={bgColor}
-            onChange={(e) => setBgColor(e.target.value)}
-          />
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">the2px.com</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Form inline>
+            <Form.Control
+              type="color"
+              value={svgColor}
+              onChange={handleSvgColorChange}
+              className="mr-2"
+            />
+            <Form.Control
+              type="color"
+              value={bgColor}
+              onChange={handleBgColorChange} // Handle background color change
+              className="mr-2"
+            />
+            <Form.Control
+              type="text"
+              placeholder="Search by tags..."
+              className="mr-2"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)} // Update search term state
+            />
+          </Form>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default CustomNavbar;
